29c572aacdc065061d548a8cbbf23f514f322458
Leonardo Barros <96589644+Leozin22@users.noreply.github.com>
2022-08-14 16:07:46 -0300

    exercicios

--- aula_8_agosto/exercicio5.cpp
+++ aula_8_agosto/exercicio5.cpp
@@ -36,7 +36,8 @@ int main(){
     
     for(int i=0; i<20; i++){
         if(i%2==0){
-            vetorC[i]=vetorA[c] c++;
+            vetorC[i]=vetorA[c] ;
+            c++;
         }
         else{
            vetorC[i]=vetorB[cont];
