9c448143088d5d0f80c591c6687c46479e41f8c5
Leonardo Barros <96589644+Leozin22@users.noreply.github.com>
2022-08-28 16:35:04 -0300

    exercicios

---
+++ fibonacci/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <ctime>
+
+using namespace std;
+
+void fibonacci(int num1, int num2){
+    
+}
+
+int main(){
+
+
+
+
+
+    return 0;
+}
